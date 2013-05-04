cf5982e4b90b6d6b2818f2ea32c6cd217ddfc749
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-05-04 20:39:57 +0200

    issue beers

--- beercoin/beercoin/views.py
+++ beercoin/beercoin/views.py
@@ -2,7 +2,9 @@
 from beercoin.util import as_json
 from django.shortcuts import get_object_or_404
 from beercoin.util.models import UserProfile, User
-
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from actstream import action
 
 def user_to_dict(user, profile=None):
     if not profile:
@@ -22,14 +24,40 @@ def check_login(request):
     return {"success": True, "username": request.user.username}
 
 
+@login_required
 @as_json
 def list_profiles(request):
     queryset = UserProfile.objects.get_visible_profiles(request.user)
     return [user_to_dict(x.user, x) for x in queryset]
 
-
+@login_required
 @as_json
 def get_profile(request, profile_name):
-    print profile_name
     user = get_object_or_404(User, username=profile_name)
     return user_to_dict(user)
+
+@transaction.commit_on_success
+@login_required
+@as_json
+def issue_beercoin(request):
+    owner = get_object_or_404(User, username=request.GET.get("owner"))
+    comment = request.GET.get("comment", None)
+    what_for = request.GET.get("what_for", None)
+    issuer = request.user
+
+    if issuer.profile.balance < -10:
+        raise ValueError()   #fixme: make better
+
+    owner.profile.balance += 1
+    issuer.profile.balance -= 1
+
+    owner.profile.save()
+    issuer.profile.save()
+
+    action.send(issuer, verb="issued", action_object=owner,
+            comment=comment, what_for=what_for)
+
+    if owner.profile.balance == -10:
+        action.send(issuer, verb="reached limit")
+
+    return {"success": True}
--- beercoin/urls.py
+++ beercoin/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^api/v1/beercoin/issue', 'beercoin.beercoin.views.issue_beercoin'),
     url(r'^api/v1/profile/$', 'beercoin.beercoin.views.list_profiles'),
     url(r'^api/v1/profile/(?P<profile_name>.*)$', 'beercoin.beercoin.views.get_profile'),
     url(r'^api/v1/check_login', 'beercoin.beercoin.views.check_login'),
