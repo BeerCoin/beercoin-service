de741dd13e742a2f6499cb070c9caa2b92dc14ef
Clumsical <wah@clumsical.com>
2013-05-04 16:41:32 +0200

    heroku db mods

--- beercoin/settings.py
+++ beercoin/settings.py
@@ -148,7 +148,7 @@ INSTALLED_APPS = (
     'beercoin.util',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    
+
 )
 
 LOGIN_REDIRECT_URL = '/accounts/%(username)s/'
@@ -190,3 +190,11 @@ LOGGING = {
         },
     }
 }
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
