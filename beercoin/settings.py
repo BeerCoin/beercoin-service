e36ff2b8cac4a5bdc6ff220d1cbdd87540a7d6e8
Clumsical <wah@clumsical.com>
2013-05-04 19:52:09 +0200

    try to fix heroku static files

--- beercoin/settings.py
+++ beercoin/settings.py
@@ -74,14 +74,14 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
+#     # Always use forward slashes, even on Windows.
+#     # Don't forget to use absolute paths, not relative paths.
+# )
 
-print STATICFILES_DIRS
+# print STATICFILES_DIRS
 
 # List of finder classes that know how to find static files in
 # various locations.
