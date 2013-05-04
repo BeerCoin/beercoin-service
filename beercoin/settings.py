81c7ba6ed3e9d68d581af9bbe86411ac8c766601
Clumsical <wah@clumsical.com>
2013-05-04 17:22:39 +0200

    Allow production and local settings swap

--- beercoin/settings.py
+++ beercoin/settings.py
@@ -191,10 +191,21 @@ LOGGING = {
     }
 }
 
+#
+# Check for a custom settings_local.py for override values on local system
+#
+if os.path.exists(os.path.join(BASE_DIR, 'settings_local.py')):
+    execfile(os.path.join(BASE_DIR, 'settings_local.py'))
+else:
+#
+# Required values for Heroku production environment
+#
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
 
-# Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#
+# End production settings
+#
