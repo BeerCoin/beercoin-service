30fad937945edf650d58af61c332c31731eda726
Clumsical <wah@clumsical.com>
2013-05-04 18:14:58 +0200

    for debugging

--- beercoin/settings.py
+++ beercoin/settings.py
@@ -206,7 +206,7 @@ else:
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    DEBUG = False
+    DEBUG = True
     TEMPLATE_DEBUG = DEBUG
 #
 # End production settings
