516a9bef0a743041fd01cc063dc6a9371bac7836
Clumsical <wah@clumsical.com>
2013-05-05 09:33:05 +0200

    email environment variables

--- beercoin/settings.py
+++ beercoin/settings.py
@@ -161,8 +161,8 @@ LOGOUT_URL = '/accounts/signout/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "in.mailjet.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("MAILJET_KEY")
-EMAIL_HOST_PASSWORD = os.environ.get("MAILJET_SECRET")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 ACTSTREAM_SETTINGS = {
