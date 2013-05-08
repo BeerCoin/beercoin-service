d54ea0d83ba7518afad1ac3050cd1d24e62f1e38
Wah <wah@clumsical.com>
2013-05-08 08:19:45 -0600

    Remove debug from production

--- beercoin/settings.py
+++ beercoin/settings.py
@@ -227,7 +227,7 @@ else:
     DATABASES['default'] = dj_database_url.config()
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    DEBUG = True
+    DEBUG = False
     TEMPLATE_DEBUG = DEBUG
 #
 # End production settings
