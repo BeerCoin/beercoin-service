ac4a9427929bd1cc57736f88d2ac831b570408a0
Wah <wah@clumsical.com>
2013-05-08 08:02:58 -0600

    here

--- beercoin/settings.py
+++ beercoin/settings.py
@@ -225,7 +225,6 @@ else:
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
-    STATIC_ROOT = '/app/beercoin/static'
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     DEBUG = True
